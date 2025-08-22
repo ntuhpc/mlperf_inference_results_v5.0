aec9efa848bc47b09fd5edc815bf1f6b32bf05ad
Zhu Zhanyan <program.nom@gmail.com>
2025-08-21 21:39:54 -0400

    fix(sdxl): remove duplicate config

--- closed/NVIDIA/configs/stable-diffusion-xl/Offline/__init__.py
+++ closed/NVIDIA/configs/stable-diffusion-xl/Offline/__init__.py
@@ -166,12 +166,3 @@ class Orin(OfflineGPUBaseConfig):
     gpu_batch_size = {'clip1': 1 * 2, 'clip2': 1 * 2, 'unet': 1 * 2, 'vae': 1}
     offline_expected_qps = 0.10
     use_graphs = True
-
-@ConfigRegistry.register(HarnessType.Custom, AccuracyTarget.k_99, PowerSetting.MaxP)
-class Coffeepot(OfflineGPUBaseConfig):
-    system = KnownSystem.coffeepot
-    precision = "fp16"
-    gpu_batch_size = {'clip1': 1 * 2, 'clip2': 1 * 2, 'unet': 1 * 2, 'vae': 1}
-    offline_expected_qps = 1
-    use_graphs = False
-    vboost_slider = 1
